8c5077860b054f27302cdbfe945c20a7c58ffaba
Wesley708 <65147671+Wesley708@users.noreply.github.com>
2023-09-15 20:19:14 -0300

    Iniciei o projeto com parte do primeiro desafio do app-idea

---
+++ script.js
@@ -0,0 +1,12 @@
+// var decimal = 0;
+
+
+
+function enviar(){
+    var decimal = document.getElementById('decimal').value;
+    calcular(decimal);
+}
+function calcular(decimal){
+    for(decimal; decimal > 0;)
+    console.log(decimal);
+}
